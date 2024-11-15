d135b7d2b923c07edc89dc063a5482d868b8f9c1
Amir <amirhoosindoosti15@gmail.com>
2024-11-15 22:30:59 +0330

    added date/time teller and system info checker

--- CLI_Runner.bat
+++ CLI_Runner.bat
@@ -1,9 +1,9 @@
 @echo off
-:: Set the title and color
+
 :START
 cls
 :: Set the title and color
-title Welcome to Amir's USB Drive!
+title Welcome to Amir's Personal CLI
 color 0A
 
 :: ASCII welcome message
@@ -13,16 +13,19 @@ echo ===========================================================
 echo.
 
 :: Pause for effect
-ping localhost -n 3 >nul
+echo Welcome!
+timeout /t 2 /nobreak >nul
 
 :: Menu
 echo What would you like to do today?
 echo [1] Open Amir's Portfolio
 echo [2] Open VsCode Installer
 echo [3] Open ToDo List
-echo [4] Play a Fun Game
-echo [5] Exit
-set /p choice=Enter your choice (1/2/3/4/5): 
+echo [4] Tell Time and Date
+echo [5] Play a Fun Game
+echo [6] Show System Info.
+echo [7] Exit
+set /p choice=Enter your choice (1/2/3/4/5/6): 
 
 if "%choice%"=="1" (
     start https://github.com/amirsenpai
@@ -37,13 +40,24 @@ if "%choice%"=="3" (
     goto START
 )
 if "%choice%"=="4" (
-    echo Starting a fun game...
-    ping localhost -n 2 >nul
-    goto game
+    timeout /t 1 /nobreak >nul
+    date /t
+    time /t
+    pause
 )
 if "%choice%"=="5" (
+    echo Starting a fun game...
+    timeout /t 2 /nobreak >nul
+    goto game
+)
+if "%choice%"=="6" (
+    systeminfo
+    timeout /t 2 /nobreak >nul
+    pause
+)
+if "%choice%"=="7" (
     echo Bye! Have a great day!
-    ping localhost -n 2 >nul
+    timeout /t 2 /nobreak >nul
     exit
 )
 echo Invalid choice. Please try again.
