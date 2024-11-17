23b7e194d0f357b5c12d713c75027d5cb1e68850
Amir <amirhoosindoosti15@gmail.com>
2024-11-17 19:53:15 +0330

    added package.json, new folder for installing node,git, updating them and making a default html/css/js folder on desktop

---
+++ installercli/Installer.bat
@@ -0,0 +1,48 @@
+@echo off
+
+:START
+cls
+echo What would you like to do?
+timeout /t 2 /nobreak >nul
+echo [1] Install Node
+echo [2] Update npm To latest Version
+echo [3] Install git {first install node}
+echo [4] Make a HTML/CSS/JS Folder
+set /p choice3=Enter Your Choice (1/2/3):
+
+if "%choice3%"=="1" (
+   start node.msi
+   echo Node installer has been opened.
+   pause
+   goto START
+)
+
+if "%choice3%"=="2" (
+   npm install -g npm@latest
+   echo npm has been updated to latest version
+   goto START
+)
+
+if "%choice3%"=="3" (
+    npm -g install git
+    pause
+    goto START
+)
+
+if "%choice3%"=="4" (
+   cd /d "%USERPROFILE%\Desktop"
+   mkdir Html_Css_Js
+   cd Html_Css_Js
+   type nul > html.html
+   type nul > css.css
+   type nul > js.js
+   echo Folder and files have been created on Desktop.
+   timeout /t 1 /nobreak >nul
+   pause
+)
+
+if not "%choice3%"=="1" if not "%choice3%"=="2" if not "%choice3%"=="3" if not "%choice3%"=="4" (
+   echo Invalid choice. Please try again.
+   timeout /t 1 /nobreak >nul
+   goto START
+)
